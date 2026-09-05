0b34fd6ea2a69d69aed6555e8fa576a5dee7ff46
TTW-F <854468573@qq.com>
2026-09-06 03:36:46 +0800

    chore: remove stray smoke-test artifacts from server/

--- server/ws_client.mjs
+++
@@ -1,5 +0,0 @@
-const ws = new WebSocket(process.argv[2]);
-ws.onmessage = e => console.log("WS-EVENT", e.data);
-ws.onopen = () => ws.send(JSON.stringify({type:"ping"}));
-ws.onclose = () => console.log("WS-CLOSED");
-setTimeout(() => process.exit(0), 9000);
